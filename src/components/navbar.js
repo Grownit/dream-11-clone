451a61547b8765c342a48028702c80c1059c7564
Grownit <tech@grownitfx.com>
2025-10-17 02:54:08 +0530

    fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -168,7 +168,7 @@ export function Navbar({ home }) {
               <img src={`${user?.image}`} alt='logo' className="logo" style={{ height: "20px", borderRadius: "50%" }} /> :
               user?.username && user?.username.charAt(0)}
           </Account>
-          <img src={`${FURL}/gcl11.png`} alt='logo' className="logo" style={{ height: "35px", width: "60px" }} />
+          <img src={`${FURL}/gclteam11.png`} alt='logo' className="logo" style={{ height: "35px", width: "60px" }} />
         </LeftSide>
         <Center>
 
